f7131231b862829cd0c3dba0f620fad409b456c9
StarkOne <Stark.pro40@gmail.com>
2019-04-25 20:38:23 +0700

    add componentDidCatch

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,36 +3,29 @@ import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ItemList from '../item-list';
 import PersonDetails from '../person-details';
-
+import PeoplePage from '../people-page';
 import './app.css';
+import ErrorIndicator from '../error-indicator';
 
 export default class App extends Component {
   state = {
-    selectedPerson: null
+    hasError: false,
   }
-  onPersonSelered = (id) => {
-
-    this.setState((state) => {
-      const { selectedPerson } = state;
-      return {
-        selectedPerson: id,
-      }
-    });
+  componentDidCatch() {
+    this.setState({ hasError: true })
+    console.log('componentDidCatch');
   }
   render() {
+    if (this.state.hasError) {
+      return <ErrorIndicator />
+    }
     return (
       <div>
         <Header />
         <RandomPlanet />
-
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList onItemSelected={this.onPersonSelered} />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson}/>
-          </div>
-        </div>
+        <PeoplePage />
+        <PeoplePage />
+        <PeoplePage />
       </div>
     );
   }
---
+++ src/components/people-page/people-page.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import ItemList from '../item-list';
+import PersonDetails from '../person-details';
+import ErrorIndicator from '../error-indicator';
+import './people-page.css';
+
+export default class PeoplePage extends Component {
+  state = {
+    selectedPerson: null,
+    hasError: false,
+  }
+  onPersonSelered = (id) => {
+    this.setState((state) => {
+      const { selectedPerson } = state;
+      return {
+        selectedPerson: id,
+      }
+    });
+  }
+
+  componentDidCatch() {
+    this.setState({
+      hasError: true
+    })
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <ErrorIndicator />
+    }
+    return (
+      <div className="row mb2">
+        <div className="col-md-6 mb-2">
+          <ItemList onItemSelected={this.onPersonSelered} />
+        </div>
+        <div className="col-md-6 mb-2">
+          <PersonDetails personId={this.state.selectedPerson} />
+        </div>
+      </div>
+    );
+  }
+}
